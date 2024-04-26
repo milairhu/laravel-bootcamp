8770018e6b5b329db69331ad37105a23defab41c
Hugo Milair <hugo.milair@gmail.com>
2024-04-26 17:57:45 +0200

    Debut bootcamp

---
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+
+        return view('chirps', [
+
+            //
+
+        ]);
+
+    }
+}
